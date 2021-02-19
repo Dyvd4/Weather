dc77e70966ff89a5befa3197c874b6b227c71d2e
Dyvd4 <david.kimmich@gmx.net>
2021-02-19 08:13:45 +0100

    node modules

--- src/Components/Wrapper.jsx
+++ src/Components/Wrapper.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
 import "../bootstrap.css";
 import "../index.css";
-import Details from './Details';
-import Preview from './Preview';
+import Details from "./Details";
+import Preview from "./Preview";
 class Wrapper extends Component {
- 
+  constructor(props) {
+    super(props);
+    this.getLocation();
+  }
   state = {};
   render() {
-    return(
-    <div className="container center">
-      <Details></Details>
-      <Preview></Preview>
-    </div>);
+    return (
+      <div className="container center">
+        <Details></Details>
+        <Preview></Preview>
+      </div>
+    );
+  }
+  getLocation() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      var { latitude, longitude } = position.coords;
+
+      this.location = {
+        latitude,
+        longitude,
+      };
+    });
   }
 }
 
